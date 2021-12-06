6c044249a991d1689c52b22a6fb9b295a2ec5de1
wangwang1994 <981128482@qq.com>
2021-12-06 13:29:28 +0800

    开始油耗运行分析

---
+++ 工作/代码/油耗及碳排放计算/运行区域分析.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import requests
+import json
+import re
+
+data = pd.read_excel('/Users/xuchangmao/Desktop/工作/代码/油耗及碳排放计算/1号周期数据.xlsx')
+AK = 'HI7l93gtaQPgYKLh58rej8iP5FDRFImP'
+
+
+def get_address(latitude, longtitude):
+    latitude = str(latitude)
+    longtitude = str(longtitude)
+    url = 'http://api.map.baidu.com/reverse_geocoding/v3/?ak={}&output=json&coordtype=wgs84ll&location={},{}'.format(AK,
+                                                                                                                     latitude,
+                                                                                                                     longtitude)
+    res = requests.get(url)
+    location = json.loads(res.text)['result']['formatted_address']
+    return location
+
+
+
+
+
