f4d6bd2c382e1d3fe24bbd8734a8ef1c325397eb
wangwang1994 <981128482@qq.com>
2021-12-06 14:34:58 +0800

    做完了轨迹的分析

--- 工作/代码/油耗及碳排放计算/运行区域信息提取.py
+++ 工作/代码/油耗及碳排放计算/运行区域信息提取.py
@@ -3,13 +3,14 @@ import requests
 import json
 import re
 
-data = pd.read_excel('/Users/xuchangmao/Desktop/工作/代码/油耗及碳排放计算/1号周期数据.xlsx')
+file_path=input('输入文件路径：')
+data = pd.read_excel(file_path)
 AK = 'HI7l93gtaQPgYKLh58rej8iP5FDRFImP'
 
 
 def get_address(latitude, longtitude):
-    latitude = str(latitude)
-    longtitude = str(longtitude)
+    latitude = str(latitude)  # 纬度
+    longtitude = str(longtitude)  # 经度
     url = 'http://api.map.baidu.com/reverse_geocoding/v3/?ak={}&output=json&coordtype=wgs84ll&location={},{}'.format(AK,
                                                                                                                      latitude,
                                                                                                                      longtitude)
@@ -18,3 +19,30 @@ def get_address(latitude, longtitude):
     return location
 
 
+def get_day_location(df):
+    # 输入一个dataframe，输出当前数据的，第一行对应的经纬度坐标的位置，作为当天的位置
+    address = get_address(df['纬度'].iloc[0], df['经度'].iloc[0])
+    return address
+
+
+def get_date(df):
+    # 输入一个dataframe,根据最开始第一行数据，提取出他的日期，形成datelist
+    date = df['日期'].iloc[0].strftime('%Y-%m-%d')
+    return date
+
+
+day_location_list = []  # 用来存储每天的位置信息
+day_list = []  # 用来存储每天的具体日期
+data['日期'] = pd.to_datetime(data['时间']).dt.date
+date_list = [x for _, x in data.groupby(data['日期'])]
+
+for item in date_list:
+    day_location_list.append(get_day_location(item))
+    day_list.append(get_date(item))
+
+result=pd.DataFrame(
+    {
+        '日期':day_list,
+        '位置':day_location_list
+    }
+                    )
